bf2ff44ccd31ee81d143ba87296d82bce472c878
Jivan Alhat <Jivan Alhat@192.168.0.6>
2021-10-06 16:25:19 +0530

    push2

--- Testing Git/src/Oct_06/Demo1.java
+++ Testing Git/src/Oct_06/Demo1.java
@@ -9,6 +9,10 @@ public class Demo1 {
 	{
 		System.out.println("Hello this is m2 method");
 	}
+	public void m2a()
+	{
+		System.out.println("Hello this is m2 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello git..");
 		Demo1 d= new Demo1();
