bd4e5d1f65d998192460f9efc6f4939f029c8d93
Jivan Alhat <Jivan Alhat@192.168.0.6>
2021-10-06 15:34:48 +0530

    by git

--- Testing Git/src/Oct_06/Demo1.java
+++ Testing Git/src/Oct_06/Demo1.java
@@ -1,8 +1,14 @@
 package Oct_06;
 
 public class Demo1 {
+	public void m1()
+	{
+		System.out.println("Hello this is m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello git..");
+		Demo1 d= new Demo1();
+		
 	}
 
 }
