99f03bd078d22ab65e5884891661dc2069c32ba0
vaishnavichawale <92014539+vaishnavichawale@users.noreply.github.com>
2021-10-06 15:41:29 +0530

    vaishnavi

--- Testing Git/src/Oct_06/Demo1.java
+++ Testing Git/src/Oct_06/Demo1.java
@@ -5,6 +5,10 @@ public class Demo1 {
 	{
 		System.out.println("Hello this is m1 method");
 	}
+	public void m2()
+	{
+		System.out.println("Hello this is m2 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello git..");
 		Demo1 d= new Demo1();
